68330714ff963f8bc067cac5e800613f8f0dd7be
ankitjain28may <ankitjain28may77@gmail.com>
2017-03-18 13:16:33 +0530

    Added social login

--- app/Http/Controllers/Social/SocialController.php
+++ app/Http/Controllers/Social/SocialController.php
@@ -47,6 +47,27 @@ class SocialController extends Controller
         return $user->getAvatar();
 
 
+        $img = Image::make('https://scontent-lht6-1.xx.fbcdn.net/v/t1.0-9/15268057_779812755489882_6749697980395923366_n.jpg?oh=9c959caa67950f6daccb614a3fbf60b6&oe=5962E962');
+
+        $height = $img->height();
+        $width = $img->width();
+
+
+        $overlay = Image::make('https://raw.githubusercontent.com/ankitjain28may/Zealicon-Profile-pic/master/dall.png')->resize(603,603);
+
+        $height1 = $overlay->height();
+        $width1 = $overlay->width();
+
+        /*return var_dump([
+            "image-h" => $height,
+            "image-w" => $width,
+            "overlay-h" => $height1,
+            "overlay-w" => $width1
+        ]);*/
+
+        $img->insert($overlay)->resize(300, 300);
+
+        return $img->response('jpg');
 
     }
 }
